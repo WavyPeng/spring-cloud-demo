b7796baa9cb248937d851cadc99e3db9a5e33f10
pengweiwei7 <pengweiwei9@jd.com>
2020-11-16 21:38:04 +0800

    init

---
+++ cloud-provider-payment/src/main/java/com/wavy/springcloud/PaymentApplication.java
@@ -0,0 +1,16 @@
+package com.wavy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author pengweiwei
+ * @Date 2020/11/16 21:27
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}

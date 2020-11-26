8514d1d7ccad4d61739ecb4e9cf34fd3cef131c6
WavyPeng <w1a9v9y3@gmail.com>
2020-11-26 21:14:29 +0800

    添加eureka集群配置

---
+++ cloud-eureka-server7002/src/main/java/com/wavy/springcloud/EurekaApplication7002.java
@@ -0,0 +1,18 @@
+package com.wavy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description
+ * @Author pengweiwei
+ * @Date 2020/11/26 20:54
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication7002.class, args);
+    }
+}
